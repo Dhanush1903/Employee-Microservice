6dfab6c3fdbbaeafed1cf605650287a3f4a5018b
unknown <cogjava3117@iiht.tech>
2022-08-18 10:43:31 +0530

    service layer is updated

---
+++ src/main/java/com/emp/service/EmployeeServiceImpl.java
@@ -0,0 +1,30 @@
+package com.emp.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.emp.entity.Employee;
+@Service
+public class EmployeeServiceImpl implements IEmployeeService{
+
+	//Dummy Employee list
+	
+	List<Employee> list= List.of(new Employee(1234L,"Tony","99784325"),
+			new Employee(1234L,"Tony","99784325"),
+			new Employee(1564L,"Sam","9978432785"),
+			new Employee(12787L,"Max","99784534325"),
+			new Employee(1239874L,"Peter","99784378625"),
+			new Employee(1234234L,"Will","99756784325"),
+			new Employee(127734L,"Bruce","909884325"),
+			new Employee(6578L,"Sofia","99786784325"));
+	
+	
+			
+	@Override
+	public Employee getEmployee(Long id) {
+		// TODO Auto-generated method stub
+		return list.stream().filter(employee-> employee.geteId().equals(id)).findAny().orElse(null);
+	}
+
+}
